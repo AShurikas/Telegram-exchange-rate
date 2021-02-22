6fe5f94ee211d74ff4669337fd137813088449eb
AShurikas <a.nechipor@gmail.com>
2021-02-22 16:27:57 +0200

    v.1

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
 import requests
-API_KEY = '?access_key=4748196e0b1c991fde8c8bb9fe9bd7f5&format=1'
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+API_KEY_stock = '?access_key=4748196e0b1c991fde8c8bb9fe9bd7f5&format=1'
+API_KEY_telegram = '1509587112:AAFRUbN-AmxgnfigSvmt_t0yskNH1mJlE_Q'
 period = {'latest': 'latest'}
-source = 'http://data.fixer.io/api/'
-r = requests.get(source+period['latest']+API_KEY)
+source = 'https://api.exchangeratesapi.io/latest?base=USD'
+r = requests.get(source)
 list_currency = ['AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTC', 'BTN', 'BWP', 'BYN', 'BYR', 'BZD', 'CAD', 'CDF', 'CHF', 'CLF', 'CLP', 'CNY', 'COP', 'CRC', 'CUC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KPW', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LTL', 'LVL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRO', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLL', 'SOS', 'SRD', 'STD', 'SVC', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VEF', 'VND', 'VUV', 'WST', 'XAF', 'XAG', 'XAU', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMK', 'ZMW', 'ZWL']
 status = r.status_code
 
 
-def start():
+
+def resp(income):
     try:
-        while True:
-            print(r.json()['rates'][(input('Введите название валюты--->>>')).upper()])
-    except (KeyboardInterrupt, EOFError):
-        print('\n Спасибо что воспользовались нашим сервисом')
+        return r.json()['rates'][income]
     except KeyError:
-        print('Неправильное запрашиваемое значение')
-        start()
+        return 'Неверно введенный параметр'
 
 
-if __name__ == '__main__':
-    start()
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help yourself')
+
+
+def rates_list(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /list is issued."""
+    for i in r.json()['rates']:
+        update.message.reply_text(i + " : " + str(round(r.json()['rates'][i], 2)))
+
+
+def run_bot(update: Update, context: CallbackContext) -> None:
+    response = resp(update.message.text.upper())
+    update.message.reply_text(response)
+    print(update.message.text)
+    print(response)
+
+
+def main():
+    """Start the bot."""
+    updater = Updater(API_KEY_telegram, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("list", rates_list))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, run_bot))
+    updater.start_polling()
+    updater.idle()
+
+
+if status == 200:
+    main()
 
 
